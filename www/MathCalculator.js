1b0b0168ab91123f22f6864f1082b098c81033f3
Senthamizh <senthamilselvan@arimaglobal.com>
2020-01-10 16:59:50 +0530

    Js Minor Changes

--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -1,4 +1,4 @@
-/*******var exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
 module.exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'MathCalculator', 'coolMethod', [arg0]);
@@ -12,8 +12,9 @@ module.exports.add = function(arg0, success, error) {
 module.exports.substract = function(arg0, success, error) {
     //exec(success CallbackFn, error CallbackFn, 'MathCalculator (Plugin Name)', 'add (Mathod Name)', [arg0](Params));
     exec(success, error, 'MathCalculator', 'substract', [arg0]);
-};*******/
+};
 
+/***
 var MathCalculator = function(){};
 
 MathCalculator.prototype.add = function(arg0, success, error){
@@ -32,3 +33,6 @@ if (!window.plugins.MathCalculator)
 
 if (typeof module != 'undefined' && module.exports)
     module.exports = MathCalculator;
+
+***/
+    
